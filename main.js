e44259cc662b078fbb770eb63e34025a0c041eba
Marsigli <marsigli1988@gmail.com>
2021-10-12 17:56:04 -0400

    missing moment and css

--- main.js
+++ main.js
@@ -1,27 +1,54 @@
-let headshots = document.querySelectorAll(".Headshot")
-console.log(headshots)
-
-let customerNames = document.querySelectorAll(".CustomerName")
-let emails = document.querySelectorAll(".email")
-let locations = document.querySelectorAll(".location")
-let DOBs = document.querySelectorAll(".DOB")
-
-//When the page loads
 //Headshot to be poplulated from customer.js to <img>
 //CustomerName to be populated from customer.js to <h3>
 //email to be populated from customer.js to <p>
 //location to be populated from customer.js to <p>
 //DOB to be populated from customer.js to <p>
 
-let counter = 0;
-window.addEventListener('load', () => {
-    for (let headshot of headshots) {
-        headshot.src = customers[counter].picture.thumbnail;
-        counter++
-    }
-})
 
-    counter = 0;
 
-    for ()
+const container = document.getElementById("container")
+console.log(container)
+
+// const customer = customers[0]
+// console.log(customer)
+
+// const card = document.createElement('div')
+
+// card.id =  customer.email
+
+// const customerName = document.createElement('h3')
+// customerName.innerText = customer.name.first + ' ' + customer.name.last
+
+// container.appendChild(card)
+
+// card.appendChild(customerName)
+
+// const customerEmail = document.createElement('p')
+// customerEmail.innerText = customer.email
+
+// card.appendChild(customerEmail)
+
+for (customer of customers) {
+    const card = document.createElement('div')
+    card.id =  customer.email
+    const customerImg = document.createElement('img')
+    customerImg.src=customer.picture.medium
+    const customerName = document.createElement('h3')
+    customerName.innerText = customer.name.first + ' ' + customer.name.last
+    container.appendChild(card)
+    card.appendChild(customerImg)
+    card.appendChild(customerName)
+    const customerEmail = document.createElement('p')
+    customerEmail.innerText = customer.email
+    card.appendChild(customerEmail)
+    const customerAddress = document.createElement('p')
+    customerAddress.innerText = customer.location.street.number + ' ' + customer.location.street.name + ' ' + customer.location.city + ' ' + customer.location.state + ' ' + customer.location.postcode
+    card.appendChild(customerAddress)
+    const customerDob = document.createElement('p')
+    // customerDob.innerText = moment(customer.dob.date).format(MMM, DD, YYYY)
+    card.appendChild(customerDob)
+}
+
+
+
 
